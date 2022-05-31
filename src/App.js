08bd8064cb3e5355129f1db314cc7ecf4d63afd5
vicuartas230 <vicuartas230@gmail.com>
2022-05-31 14:20:15 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import Boton from './componentes/Boton';
+import Contador from './componentes/Contador';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
+import { useState } from 'react';
+
+function App() {
+
+  const [numClicks, setNumClicks] = useState(0);
+
+  const manejarClick = () => {
+    setNumClicks(numClicks + 1);
+  }
+
+  const reiniciarContador = () => {
+    setNumClicks(0);
+  }
+
+  return (
+    <div className="App">
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+        className='freecodecamp-logo'
+        src={freeCodeCampLogo}
+        alt='Logo de freeCodeCamp'
+        />
+      </div>
+      <div className='contenedor-principal'>
+        <Contador numClicks={numClicks} />
+        <Boton 
+          texto='Click'
+          esBotonDeClick={true}
+          manejarClick={manejarClick}/>
+        <Boton 
+          texto='Reiniciar'
+          esBotonDeClick={false}
+          manejarClick={reiniciarContador}/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
